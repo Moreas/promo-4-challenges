60bf50f3d88d4a2d45cf8fbe9359bb685f2ef5f5
Florent Pignaud <florent.pignaud@gmail.com>
2014-10-08 17:14:24 +0200

    exercice 5 clean

--- 01-Ruby/02-Arrays-Enumerable/05-Destructive-Methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/05-Destructive-Methods/lib/pmu.rb
@@ -1,7 +1,6 @@
 def pmu_format!(race_array)
   # TODO: modify the given array so that it is PMU-consistent. This method should return nil.
+  race_array.map! {|a| "#{race_array.index(a) + 1}-#{a}!"}.reverse!
 end
 
-
-
-
+# => p pmu_format!(["Abricot du Laudot", "Black Caviar", "Brigadier Gerard"])
