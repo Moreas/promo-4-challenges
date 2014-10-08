36530eaf5dac9ae52e826f1f5d94e76397cb855c
Florent Pignaud <florent.pignaud@gmail.com>
2014-10-08 14:13:03 +0200

    version finale sans gestion erreur

--- 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
@@ -1,32 +1,20 @@
-<<<<<<< HEAD
 def sum_with_while(min,max)
-  i=min
-  sum=0def sum_recursive(min,max)
-  #TODO (optional): return the sum of the integers between min and max
-  #CONSTRAINT: you should use a recursive algorithm, i.e. the method should call itself
-
-  until min == max
-      sum = min + 1
-      min = min + 1
-      return sum_recursive(sum, max)
-  end
-
-end
-  while i <= max
-    sum = sum + i
-    i = i+1
+  #TODO: return the sum of the integers between min and max
+  #CONSTRAINT: you should use a while..end structure
+  sum = 0
+  while (min <= max)
+    sum = sum + min
+    min = min + 1
   end
   return sum
-    #TODO: return the sum of the integers between min and max
-  #CONSTRAINT: you should use a while..end structure
 end
 
 def sum_with_for(min,max)
   #TODO: return the sum of the integers between min and max
   #CONSTRAINT: you should use a for..end structure
   sum = 0
-  for i in  (min..max)
-  sum = sum + i
+  for i in (min..max)
+    sum = sum + i
   end
   return sum
 end
@@ -34,24 +22,11 @@ end
 def sum_recursive(min,max)
   #TODO (optional): return the sum of the integers between min and max
   #CONSTRAINT: you should use a recursive algorithm, i.e. the method should call itself
-
-  until min == max
-      sum = min + 1
-        return sum_recursive(sum, max)
-      min = min + 1
-  end
-
-=======
-def sum_with_while(min, max)
-  # CONSTRAINT: you should use a while..end structure
+  if (min > max)
+    puts "Please make sure min < max"
+    elsif (min < max)
+      min = min + sum_recursive(min + 1 , max)
+    else
+      return min
+    end
 end
-
-def sum_with_for(min, max)
-  # CONSTRAINT: you should use a for..end structure
-end
-
-def sum_recursive(min, max)
-  # CONSTRAINT: you should use a recursive algorithm, i.e. the method should call itself
->>>>>>> c7dc7e6a03f18e26e6959fb11e05dc7bed22003b
-end
-
