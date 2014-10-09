684f485a892b41594860cf4abe41499a63d1d06c
Florent Pignaud <florent.pignaud@gmail.com>
2014-10-09 11:41:40 +0200

    fin exo 2

--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -1,8 +1,13 @@
 def tag(tag_name, attr = nil)
   # TODO:  Build HTML tags around  content given in the block
   #        The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
-end
+"<#{tag_name}#{" #{attr[0]}" if attr != nil}#{ "='#{attr[1]}'" if attr != nil}>#{yield}</#{tag_name}>"end
+
+
 
 def timer_for
   # TODO:  Return time taken to execute the given block
+  start_time = Time.now
+  yield
+  Time.now - start_time
 end
