b8b6b98f77cb8bf4cc09318c653ae67663e82ce0
Florent Pignaud <florent.pignaud@gmail.com>
2014-10-10 09:13:15 +0200

    premier draft louchebenm

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -2,4 +2,35 @@
 
 def louchebemize(sentence)
   # TODO: implement your louchebem translator
+
+vowel = ['a','e','i','o','u','y']
+
+  if sentence.size > 1
+    if is_vowel?(sentence[0])
+      sentence.insert(0,'l')
+    else
+      sentence.split("").each_with_index do |l,i|
+        if (!is_vowel?(l))
+          sentence.delete_at(i)
+        end
+          sentence.insert(0,'l')
+      end
+    end
+  end
+  return sentence
 end
+
+
+public def is_vowel?(letter)
+vowel = ['a','e','i','o','u','y']
+return vowel.include?(letter)
+end
+
+puts ("Say a word")
+sentence = gets.chomp
+puts louchebemize(sentence)
+# if is_vowel?(sentence[0])
+#   puts "voyelle"
+# else
+#   puts "consonne"
+# end
