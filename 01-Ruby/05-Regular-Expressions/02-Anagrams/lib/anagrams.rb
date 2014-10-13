8c57011a392b3ad9355404ec3bbe18bb7514c510
Florent Pignaud <florent.pignaud@gmail.com>
2014-10-13 19:47:35 +0200

    version finale exo 2

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,37 +1,20 @@
 def anagrams?(a_string, another_string)
   # TODO: implement the obvious method to test if two words are anagrams
-  debut = Time.now
   a_string  = a_string.gsub(/\W/, "")
   another_string = another_string.gsub(/\W/, "")
-  anagram = false
-  if a_string.upcase.split("").sort.join == another_string.upcase.split("").sort.join
-    anagram = true
-  end
-  fin = Time.now
-  p fin - debut
-  return anagram
+  (a_string.upcase.split("").sort.join == another_string.upcase.split("").sort.join) ? (return true) : (return false)
 end
 
 def anagrams_on_steroids?(a_string, another_string)
   # TODO: implement the improved method
-  debut = Time.now
   a_string  = a_string.gsub(/\W/, "")
   another_string = another_string.gsub(/\W/, "")
-  anagram = false
-  i = 0
   w1 = Hash.new(0)
   w2 = Hash.new(0)
-  if a_string.size == another_string.size
-    while ((i + 1) <= a_string.size)
-      w1[a_string[i].downcase] = w1[a_string[i].downcase] + 1
-      w2[another_string[i].downcase] = w2[another_string[i].downcase] + 1
-      i = i + 1
-    end
-    if w1.eql?(w2)
-      anagram = true
-    end
-  end
-  fin = Time.now
-  p fin - debut
-  return anagram
+  a_string.split("").each { |l| w1[a_string[l].downcase] = w1[a_string[l].downcase] + 1 }
+  another_string.split("").each { |l| w2[another_string[l].downcase] = w2[another_string[l].downcase] + 1 }
+  (w1.eql?(w2)) ? (anagram = true) : (anagram = false)
+  return ((a_string.size == another_string.size) & anagram)
 end
+
+
