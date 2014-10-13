5c30fbe4312ad0a49938d853924f2da947934b18
Florent Pignaud <florent.pignaud@gmail.com>
2014-10-13 11:34:09 +0200

    fin exo 1

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,9 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  phone_number = phone_number.gsub(/( |-)/,"")
+  if phone_number =~ /^(0|\+33)[^0]\d{8}$/
+    true
+  else
+    false
+  end
 end
