d9503aba37ad0ee986f57f1b9e619c14bbd9edc1
Florent Pignaud <florent.pignaud@gmail.com>
2014-10-14 18:19:26 +0200

    fin exo 2 no offence

--- 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
+++ 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
@@ -10,28 +10,32 @@ end
 
 def run_game(attempt, grid, start_time, end_time)
   # TODO: runs the game and return detailed hash of result
-  attempt = attempt.upcase
-  result = JSON.parse(open("http://api.wordreference.com/0.8/80143/json/enfr/#{attempt}").read)
+  dur = end_time - start_time
+  correct = included_array(attempt.upcase.split(""), grid.flatten)
+  if translate(attempt).nil?
+    return { time: dur, translation: nil, score: 0, message: "not an english word" }
+  elsif correct
+    return { time: dur, translation: translate(attempt), score: attempt.size * 10 - (dur).to_i, message: "well done" }
+  else
+    return { time: dur, translation: nil, score: 0, message: "not in the grid" }
+  end
+end
+
+def translate(word)
+  result = JSON.parse(open("http://api.wordreference.com/0.8/80143/json/enfr/#{word}").read)
+  if result["Error"]
+    return nil
+  else
+    return result["term0"]["PrincipalTranslations"]["0"]["FirstTranslation"]["term"]
+  end
+end
+
+def included_array(a1, a2)
   correct = true
-  score = attempt.size * 10 - (end_time - start_time).to_i
   i = 0
-  while correct && (i < attempt.size)
-    grid.flatten.include?(attempt.split("")[i]) ? correct = true : correct = false
+  while correct && (i < a1.size)
+    a2.include?(a1[i]) ? correct = true : correct = false
     i += 1
   end
-  if result["Error"] == "NoTranslation"
-    score = 0
-    message = "not an english word"
-    translation = nil
-  else
-    if correct
-      translation = result["term0"]["PrincipalTranslations"]["0"]["FirstTranslation"]["term"]
-      message = "well done"
-    else
-      score = 0
-      message = "not in the grid"
-      translation = nil
-    end
-  end
-  return { time: end_time - start_time, translation: translation, score: score, message: message }
+  return correct
 end
