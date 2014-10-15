655be8be2620f69e2eb3bed16c743b343b194680
Florent Pignaud <florent.pignaud@gmail.com>
2014-10-15 11:23:12 +0200

    Fin exo 1

--- 01-Ruby/02-Flow-Array/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Flow-Array/03-Black-Jack/lib/croupier.rb
@@ -7,9 +7,9 @@ end
 
 def end_game_message(player_score, bank_score)
   # TODO: return (not print!) a message telling if the user won or lost.
-  if (player_score == 21)
+  if player_score == 21
     "black jack"
-  elsif  ((player_score > bank_score) && (player_score < 21))
+  elsif  (player_score > bank_score) && (player_score < 21)
     "You beat the bank! You win."
   else
     "The bank beats you! You lose."
@@ -20,17 +20,13 @@ end
 def play_game
   # TODO: make the user play from terminal in a while loop that will stop
   #       when the user will not be asking for  a new card
-
   bank_score = pick_bank_score
   player_score = 0
   puts state_of_the_game(player_score, bank_score)
-
   puts "Card ? (type 'y' or 'yes' for a new card)"
-
   answer = gets.chomp
-
-  while (answer == 'y' || answer == 'yes')
-    player_score = player_score + pick_player_card
+  while answer == 'y' || answer == 'yes'
+    player_score += pick_player_card
     puts state_of_the_game(player_score, bank_score)
     if player_score > 21
       answer = "out"
