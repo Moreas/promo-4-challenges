75617f1261e0bb091d656642d02d675782eb3498
Florent Pignaud <florent.pignaud@gmail.com>
2014-10-16 16:36:28 +0200

    fin exo 3

--- 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
+++ 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
@@ -1,21 +1,25 @@
 class Animal
   def name(name)
     @name = name
+    return self
     # TODO: find good instruction
   end
 
   def specie(specie)
     @specie = specie
+    return self
     # TODO: find good instruction
   end
 
   def color(color)
     @color = color
+    return self
     # TODO: find good instruction
   end
 
   def natural_habitat(natural_habitat)
     @natural_habitat = natural_habitat
+    return self
     # TODO: find good instruction
   end
 
