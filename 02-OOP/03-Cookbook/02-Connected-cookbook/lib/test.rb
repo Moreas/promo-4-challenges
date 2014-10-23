a0e3ae2f05733c77f63d6d9594db1c73e3babd00
Florent Pignaud <florent.pignaud@gmail.com>
2014-10-23 17:04:44 +0200

    fin food delivery et debut DB

--- 02-OOP/03-Cookbook/02-Connected-cookbook/lib/test.rb
+++ 02-OOP/03-Cookbook/02-Connected-cookbook/lib/test.rb
@@ -1,12 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-puts "Que cherchez-vous?"
-search = gets.chomp
-response = open("http://www.marmiton.org/recettes/recherche.aspx?aqt=#{search}")
+response = open("https://transferwise.com/")
 
 doc = Nokogiri::HTML(File.open(response), nil, 'utf-8')
- doc.search('.m_contenu_resultat').each do |element|
-  puts "Nom: #{element.search('.m_titre_resultat > a').inner_text}"
-  puts "Description: #{element.search('.m_texte_resultat').inner_text}"
-  end
+ # doc.search('.b-calculator__details results js-b-calculator-details').each do |element|
+ #  puts "Rate Info: #{element.search('.b-calculator__details-transferwise')}"
+ #  end
+ p doc.css('.b-calculator__details-transferwise > p').inner_text.split(" ")
