a7bfc51d619dc0988429e2bd241795f04ebe9393
Florent Pignaud <florent.pignaud@gmail.com>
2014-10-27 12:20:18 +0100

    fin exo 3

--- 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
@@ -1,3 +1,12 @@
 require "faker"
 
 # TODO: Write a seed to insert 100 posts in the database
+
+100.times do
+  post = Post.new(
+    name: Faker::Lorem.word,
+    url: Faker::Internet.url,
+    votes: (0..100).to_a.sample
+  )
+  post.save
+end
