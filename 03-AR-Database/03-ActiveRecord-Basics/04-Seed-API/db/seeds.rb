948d9559e714f4e00b54521d559213751be3a6e7
Florent Pignaud <florent.pignaud@gmail.com>
2014-10-27 15:31:48 +0100

    fin exo 4

--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/db/seeds.rb
@@ -1,4 +1,16 @@
 require "json"
 require "rest-client"
 
-# TODO: Write a seed to insert 100 posts in the database fetched from the Hacker News API.
+response = JSON.parse(RestClient.get 'https://hacker-news.firebaseio.com/v0/topstories.json')
+response[0..9].each do |i|
+  uri = "https://hacker-news.firebaseio.com/v0/item/#{i}.json"
+  post_info = JSON.parse(RestClient.get uri)
+  post = Post.new(
+  name: post_info[:title],
+  url: post_info[:url],
+  votes: post_info[:score]
+    )
+  post.save
+end
+
+# TODO: Write a seed to insert 100 posts in the database fetched from the Hacker News API.}
