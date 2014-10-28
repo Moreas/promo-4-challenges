257ec2d1d1f3327f1940908136c02e3d6041639d
Florent Pignaud <florent.pignaud@gmail.com>
2014-10-28 12:17:17 +0100

    exo 2 en cours

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-
   # TODO: Add some validation
+  validates :user, :name, :url, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :name, length: { minimum: 5 }
+  validates :url, format: { with: /\A.*\..*\z/ }
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_many :posts
-
   # TODO: Add some validation
+  validates :username, :email, presence: true
+  validates :username, uniqueness: true
+  validates :email, format: { with: /\A.*@.*\..*\z/ }
 end
