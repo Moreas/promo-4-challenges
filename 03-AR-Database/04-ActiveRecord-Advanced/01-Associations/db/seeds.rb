c067248cc68b579efffae53626f2b261bd819c8c
Florent Pignaud <florent.pignaud@gmail.com>
2014-10-28 11:08:39 +0100

    fin exo 1

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,20 @@
 # TODO: Write a seed
+require "faker"
+
+5.times do
+  user = User.new(
+    username: Faker::Name.name,
+    email: Faker::Internet.email
+  )
+  (rand(6) + 5).times do
+    post = Post.new(
+    name: Faker::Lorem.word,
+    url: Faker::Internet.url,
+    votes: (0..100).to_a.sample
+      )
+    post.user = user
+    post.save
+  end
+  user.save
+end
+
